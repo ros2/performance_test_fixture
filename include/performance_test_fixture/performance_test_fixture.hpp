0324bd71891a530e2bc5f71cb94d68c111038702
Scott K Logan <logans@cottsay.net>
2020-07-16 13:51:37 -0700

    Initial 'performance_test_fixture' package
    Signed-off-by: Scott K Logan <logans@cottsay.net>

---
+++ include/performance_test_fixture/performance_test_fixture.hpp
@@ -0,0 +1,44 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PERFORMANCE_TEST_FIXTURE__PERFORMANCE_TEST_FIXTURE_HPP_
+#define PERFORMANCE_TEST_FIXTURE__PERFORMANCE_TEST_FIXTURE_HPP_
+
+#include <benchmark/benchmark.h>
+#include <osrf_testing_tools_cpp/memory_tools/memory_tools.hpp>
+
+#include "performance_test_fixture/visibility_control.hpp"
+
+namespace performance_test_fixture
+{
+
+class PerformanceTest : public ::benchmark::Fixture
+{
+public:
+  PerformanceTest();
+
+  void SetUp(::benchmark::State & state);
+  void TearDown(::benchmark::State & state);
+
+protected:
+  void on_malloc(osrf_testing_tools_cpp::memory_tools::MemoryToolsService & service);
+  void on_realloc(osrf_testing_tools_cpp::memory_tools::MemoryToolsService & service);
+
+private:
+  size_t allocation_count;
+};
+
+}  // namespace performance_test_fixture
+
+#endif  // PERFORMANCE_TEST_FIXTURE__PERFORMANCE_TEST_FIXTURE_HPP_
