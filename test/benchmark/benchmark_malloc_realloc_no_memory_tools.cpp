c8a1ae02dddd4eb8f2addabb77f907a835332f25
miguelgonrod <miguelgonrod2004@gmail.com>
2026-05-25 14:41:40 -0500

    Added macros to check benchmark version
    Signed-off-by: miguelgonrod <miguelgonrod2004@gmail.com>

--- test/benchmark/benchmark_malloc_realloc_no_memory_tools.cpp
+++ test/benchmark/benchmark_malloc_realloc_no_memory_tools.cpp
@@ -18,6 +18,19 @@
 
 #include "./macros.h"
 
+namespace benchmark_compat
+{
+
+#if defined(BENCHMARK_VERSION_MAJOR) && \
+  ((BENCHMARK_VERSION_MAJOR > 1) || \
+  (BENCHMARK_VERSION_MAJOR == 1 && BENCHMARK_VERSION_MINOR >= 9))
+using ApplyBenchmark = benchmark::Benchmark;
+#else
+using ApplyBenchmark = benchmark::internal::Benchmark;
+#endif
+
+}  // namespace benchmark_compat
+
 // This does not make use of PauseTiming or ResumeTiming because timing is very short for these
 // benchmarks. However, they should allow for comparisons to the other benchmark_malloc_realloc
 static void benchmark_on_malloc(benchmark::State & state)
@@ -89,7 +102,7 @@ BENCHMARK(benchmark_on_calloc)->ArgNames({"Alloc Size"})->RangeMultiplier(16)->R
 // Three types of realloc tests, one where malloc is smaller than realloc, one where they are
 // the same, and one where malloc is larger than realloc. Realloc size ranges from 1 to 2^27
 // each time multiplying by 32.
-static void realloc_args(benchmark::Benchmark * b)
+static void realloc_args(benchmark_compat::ApplyBenchmark * b)
 {
   for (int64_t malloc_adjustment = -1; malloc_adjustment <= 1; ++malloc_adjustment) {
     for (int64_t realloc_shift = 0; realloc_shift < 32; realloc_shift += 8) {
